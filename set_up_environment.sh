9406df3d8429b57264be2add3cb4b82d2175b952
Yehor Solonukha <yehor@student.kul.edu.pl>
2025-12-27 22:47:53 +0100

    added automatic environment set-up, three terminal tabs with 2 clients and 1 server

---
+++ set_up_environment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wt.exe \
+        new-tab --title "Server" wsl bash -c "docker compose run --rm server" \
+        ';'  new-tab --title "Client 1" wsl bash -c "docker compose run --rm client1" \
+        ';' new-tab --title "Client 2" wsl bash -c "docker compose run --rm client2"
